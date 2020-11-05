200088abf6ed77242537462462e52349abf155e4
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-11-05 19:03:44 +0800

    store

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
  */
 'use strict';
 global.CODE = require('./app/const/statusCode');
+const fs = require('fs');
+const path = require('path');
+const lowdb = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const utils = require('./app/utils/utils');
 
 class AppBootHook {
   constructor(app) {
@@ -33,6 +38,19 @@ class AppBootHook {
   async didReady() {
     // Worker is ready, can do some things
     // don't need to block the app boot.
+    // 数据库
+    const storageDir = path.normalize('./storage/');
+    if (!fs.existsSync(storageDir)) {
+      utils.mkdir(storageDir);
+      utils.chmodPath(storageDir, '777');
+    }
+    const file = storageDir + 'db.json';
+    // utils.chmodPath(file);
+    const adapter = new FileSync(file);
+    const db = lowdb(adapter);
+    if (!db.has('kv').value()) {
+      db.set('kv', {}).write();
+    }
   }
 
   async serverDidReady() {
