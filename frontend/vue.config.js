853015660fe6b48b154957e09248fc3d62c303e3
gaoshuaixing <gaoshuaixing@bilibili.com>
2021-02-03 15:45:20 +0800

    1.7.0

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     //Solution For Issue:You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
     //zhengkai.blog.csdn.net
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    configureWebpack: (config) => {
+      config["performance"] = {
+        "maxEntrypointSize": 10000000,
+        "maxAssetSize": 30000000,
+      }
+    }
   }
