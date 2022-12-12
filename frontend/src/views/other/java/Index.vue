8446e750a2aaaf835526fc8953db0e8a37144fce
zuihou <244387066@qq.com>
2022-12-12 23:20:38 +0800

    feat: 新增启动程序的同时启动额外资源目录中的jar
    仅测试了window和mac系统
    
    1. ee 框架ready后，通过命令启动jar(优先试用配置的端口，被占用时则随机端口)
    2. did-finish-load 事件通知 fronend 存储java程序的访问地址
    3. 程序退出时，通过命令行kill掉程序
    4. 提供前端调用 java 接口的示例

--- frontend/src/views/other/java/Index.vue
+++ frontend/src/views/other/java/Index.vue
@@ -3,22 +3,34 @@
     <div class="one-block-1">
       <span>
         请求java后台接口， 本示例需要修改如下： <br/>
-        1. 修改 electron/config/config.default.js 中 config.javaServer.enable = true <br/>
-        2. 官方下载 jre 并解压到： build/extraResources <br/>
-        3. 编译 spring boot 可执行jar到： build/extraResources <br/>
-
-        下载我准备好的 jre 和 app.jar 看效果： <br/>
-
-        链接: https://pan.baidu.com/s/1QLtFC76uD6_dm01S6xaUSA  密码: cqpf
-        <br/>
-        同时，你可以将18080端口先占用，后启动ee程序，观察请求的端口
-
       </span>
     </div>  
     <div class="one-block-2">
       <a-space>
         <a-button @click="exec(1111111)"> 点击 </a-button>
       </a-space>
+    </div>
+    <div class="one-block-2">
+      <span>
+        1. 修改 electron/config/config.default.js 中 config.javaServer.enable = true <br/>
+        2. 官方下载 jre 并解压到： build/extraResources <br/>
+        3. 编译 spring boot 可执行jar到： build/extraResources <br/>
+
+        下载我准备好的 jre 和 app.jar 看效果： <br/>
+        链接: https://pan.baidu.com/s/1QLtFC76uD6_dm01S6xaUSA  密码: cqpf   <br/>
+        注意： 请根据你的操作系统，选择正确的jre进行下载 <br/>
+
+        同时可以去oracle官方下载其他版本的jre： <br/>
+
+        https://www.oracle.com/java/technologies/javase/javase8-archive-downloads.html
+        <br/>
+
+        <img src="~@/assets/java.png"/>
+
+        <br/>
+        同时，你可以将18080端口先占用，后启动ee程序，观察请求的端口
+
+      </span>
     </div>
   </div>
 </template>
