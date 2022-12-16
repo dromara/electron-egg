a31ceb522c9b2e26c83cbaa51848447eb056c673
gaoshuaixing <gaoshuaixing@bilibili.com>
2022-12-16 16:50:30 +0800

    注释说明

--- electron/config/encrypt.js
+++ electron/config/encrypt.js
@@ -14,9 +14,9 @@ module.exports = {
   confusionOptions: {
     compact: true, // 将代码压缩为1行        
     stringArray: true, // 删除字符串文本并将其放置在特殊数组中
-    stringArrayEncoding: ['none'], // 对stringArray编码 'none', 'base64', 'rc4'，注意：会增加代码大小，降低运行速度
+    stringArrayEncoding: ['none'], // 对stringArray编码 'none', 'base64', 'rc4'，增加安全性
     disableConsoleOutput: true, // 禁止console输出
     deadCodeInjection: false, // 是否注入死代码
-    debugProtection: false // 是否允许debug代码
+    debugProtection: true // 是否禁用debug
   }
 };
