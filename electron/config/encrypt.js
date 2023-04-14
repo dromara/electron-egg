8907b76a1735fe8caa8d834367c4ab47548aeb73
哆啦好梦 <530353222@qq.com>
2023-04-14 16:40:06 +0800

    3.1.0

--- electron/config/encrypt.js
+++ electron/config/encrypt.js
@@ -1,10 +1,16 @@
 /**
  * 加密配置
+ * @param type - confusion | bytecode | strict
  */
 module.exports = {
-  type: 'bytecode',
-  directory: [
-    'electron'
+  type: 'confusion',
+  files: [
+    'electron/**/*.(js|json)',
+    '!electron/config/encrypt.js',
+    '!electron/config/nodemon.json'
+  ],
+  cleanFiles: [
+    'electron',
   ],
   fileExt: ['.js'],
   confusionOptions: {
--- main.js
+++ main.js
@@ -1,51 +1,5 @@
-const { Application } = require('ee-core');
 const EE = require('ee-core/ee');
-
-class Main extends Application {
-
-  constructor() {
-    super();
-    // this === eeApp;
-  }
-
-  /**
-   * core app have been loaded
-   */
-  async ready () {
-    // do some things
-  }
-
-  /**
-   * electron app ready
-   */
-  async electronAppReady () {
-    // do some things
-  }
-
-  /**
-   * main window have been loaded
-   */
-  async windowReady () {
-    // do some things
-    // 延迟加载，无白屏
-    const winOpt = this.config.windowsOption;
-    if (winOpt.show == false) {
-      const win = this.electron.mainWindow;
-      win.once('ready-to-show', () => {
-        win.show();
-      })
-    }
-  }
-
-  /**
-   * before app close
-   */  
-  async beforeClose () {
-    // do some things
-
-  }
-}
+const App = require('./electron');
 
 // Instantiate an app object
-EE.app = new Main();
- 
+EE.app = new App();
