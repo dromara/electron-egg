575140481c3830eed203254de5c8b3f1ebf2d688
gsx <530353222@qq.com>
2023-06-14 08:53:52 +0800

    过滤builder文件

--- electron/config/encrypt.js
+++ electron/config/encrypt.js
@@ -7,7 +7,8 @@ module.exports = {
   files: [
     'electron/**/*.(js|json)',
     '!electron/config/encrypt.js',
-    '!electron/config/nodemon.json'
+    '!electron/config/nodemon.json',
+    '!electron/config/builder.json',
   ],
   fileExt: ['.js'],
   confusionOptions: {
