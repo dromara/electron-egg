ae93b0542e4631ca8436d36d36f7aa4012b295e0
gsx <530353222@qq.com>
2023-07-03 21:42:46 +0800

    3.5.1

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     //Solution For Issue:You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build.
     //zhengkai.blog.csdn.net
+    publicPath: './', // 相对路径，file模式和web模式都可以使用
     runtimeCompiler: true,
     configureWebpack: (config) => {
       config["performance"] = {
