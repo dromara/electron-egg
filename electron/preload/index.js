e237eaea564a6d3eccae59af0192632c3882e541
哆啦好梦 <530353222@qq.com>
2023-07-19 14:27:52 +0800

    fromat

--- electron/preload/index.js
+++ electron/preload/index.js
@@ -1,16 +1,16 @@
 /*************************************************
  ** preload为预加载模块，该文件将会在程序启动时加载 **
  *************************************************/
- const Addon = require('ee-core/addon');
+const Addon = require('ee-core/addon');
 
- /**
-  * 预加载模块入口
-  */
- module.exports = async () => {
- 
-   // 示例功能模块，可选择性使用和修改
-   Addon.get('tray').create();
-   Addon.get('security').create();
-   Addon.get('awaken').create();
-   Addon.get('autoUpdater').create();
- }
+/**
+* 预加载模块入口
+*/
+module.exports = async () => {
+
+  // 示例功能模块，可选择性使用和修改
+  Addon.get('tray').create();
+  Addon.get('security').create();
+  Addon.get('awaken').create();
+  Addon.get('autoUpdater').create();
+}
