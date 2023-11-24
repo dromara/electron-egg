9c462e13a23c5de87237892d081379e95f4401e6
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-11-24 14:39:01 +0800

    go example

--- go/api/example.go
+++ go/api/example.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"ee-go/eapp"
 	"ee-go/ehelper"
 	"ee-go/ehttp/router"
+	"ee-go/elog"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,15 +15,19 @@ import (
 // 使用 router Ctx
 func Hello(c *router.Ctx) {
 	ret := ehelper.GetJson()
-	ret.Data = "hello electron-egg"
-
 	defer c.JSON(ret)
+
+	ret.Data = "hello electron-egg"
+	elog.Logger.Info(" print Hello ")
+	time.Sleep(5 * time.Second)
 }
 
 // 使用 gin Context
 func Info(gc *gin.Context) {
 	ret := ehelper.GetJson()
 	defer gc.JSON(http.StatusOK, ret)
+
+	elog.Logger.Info(" print info ")
 }
 
 func Exit(c *router.Ctx) {
