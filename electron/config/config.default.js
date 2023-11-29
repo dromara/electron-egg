ac7b4056f85ed188c636c9c077c8c8956317ed0f
gaoshuaixing <gaoshuaixing@bilibili.com>
2023-11-29 17:16:01 +0800

    del config

--- electron/config/config.default.js
+++ electron/config/config.default.js
@@ -129,7 +129,7 @@ module.exports = (appInfo) => {
     go: {
       enable: true,
       name: 'goapp',
-      args: ['--env=dev', '--port=7073'],
+      args: ['--port=7073'],
       appExit: false,
     }
   };   
