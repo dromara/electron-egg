15961a5bc5bdba6824a4b9c36c264f21cca6ce80
gaoshuaixing <gaoshuaixing@bilibili.com>
2024-03-07 19:08:55 +0800

    python ig

--- electron/config/config.local.js
+++ electron/config/config.local.js
@@ -48,7 +48,7 @@ module.exports = (appInfo) => {
       cmd: 'python',
       directory: './python',
       args: ['./main.py', '--port=7074'],
-      stdio: "inherit",
+      stdio: "ignore",
       appExit: true,
     },
   };   
