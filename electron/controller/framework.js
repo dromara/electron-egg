cce0d908dad649c0046eb8694151e1e0ab0d1bca
gaoshuaixing <gaoshuaixing@bilibili.com>
2024-07-30 16:46:10 +0800

    优化启动路径

--- electron/controller/framework.js
+++ electron/controller/framework.js
@@ -117,6 +117,7 @@ class FrameworkController extends Controller {
 
   /**
    * 调用其它程序（exe、bash等可执行程序）
+   * 
    */
   openSoftware(softName) {
     if (!softName) {
@@ -130,9 +131,12 @@ class FrameworkController extends Controller {
     if (!fs.existsSync(softwarePath)) {
       return false;
     }
-    // 命令行字符串 并 执行
-    let cmdStr = 'start ' + softwarePath;
+    // 命令行字符串 并 执行, start 命令后面的路径要加双引号
+    let cmdStr = `start "${softwarePath}"`;
     exec(cmdStr);
+
+    // 方法二
+    // 推荐使用cross模块
 
     return true;
   }  
