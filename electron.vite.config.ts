9373db5a372200b2c73eb8268d7a6ba1029800d8
gaoshuaixing <530353222@qq.com>
2025-01-02 08:26:53 +0800

    chore: ts config

---
+++ electron.vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  // preload: {
+  //   plugins: [externalizeDepsPlugin()]
+  // },
+})
