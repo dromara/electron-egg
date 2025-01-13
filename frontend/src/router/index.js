c83c57e618cb4912c34bcfb8b466dec3e875e40a
gaoshuaixing <gaoshuaixing@bilibili.com>
2025-01-13 18:21:22 +0800

    delete: clean code

--- frontend/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import routerMap from './routerMap'
-
-const Router = createRouter({
-  history: createWebHashHistory(),
-  routes: routerMap,
-})
-
-export default Router
