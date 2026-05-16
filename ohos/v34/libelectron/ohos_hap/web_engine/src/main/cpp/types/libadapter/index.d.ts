3f25d6fddd1bc27851941cb04b3e8031756eb9a0
gaoshuaixing <530353222@qq.com>
2026-05-16 11:08:21 +0800

    del

--- ohos/v34/libelectron/ohos_hap/web_engine/src/main/cpp/types/libadapter/index.d.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import type { NativeContext } from '../../../ets/interface/CommonInterface';
-
-export const getNativeContext: (contextType: number) => NativeContext;
-export const SetContextPaths: (paths: Record<string, string>) => void;
