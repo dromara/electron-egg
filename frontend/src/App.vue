77a41281b9ddbfba2ab703198f66dd4d236f077f
wallace5303 <530353222@qq.com>
2026-05-24 16:29:23 +0800

    demo 前端依赖升级

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-    <router-view/>
+    <a-config-provider :theme="theme">
+        <router-view/>
+    </a-config-provider>
 </template>
 <script setup>
 import { onMounted } from 'vue';
+import theme from './theme';
 
 onMounted(() => {
   const loadingElement = document.getElementById('loadingPage');
